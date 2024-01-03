9de514728f33bbc939a5dad2fa7f24d707963607
Lighttouch <hyunk8282@gmail.com>
2024-01-03 11:27:25 +0900

    change .env to local variable

--- app/_component/CTitle.tsx
+++ app/_component/CTitle.tsx
@@ -1,6 +1,8 @@
+import {APP_LAST_UPDATE, APP_VERSION} from "@/app/value";
+
 export default function CTitle() {
     return <div className={'flex flex-col justify-center items-center text-3xl font-medium mb-8'}>
         <div>Lotto Drawing Probability Simulator</div>
-        <div className={'text-base font-light mt-2'}>{`v${process.env.NEXT_PUBLIC_VERSION} (${process.env.NEXT_PUBLIC_LAST_UPDATE})`}</div>
+        <div className={'text-base font-light mt-2'}>{`v${APP_VERSION} (${APP_LAST_UPDATE})`}</div>
     </div>
 }
